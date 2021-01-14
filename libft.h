36562fcb3c283a1c6f351daddc8efe23a9728179
Romeu - ABALMAR <romeumartinscarvalho@gmail.com>
2021-01-14 10:38:40 +0000

    ft_strtrim

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rmartins <rmartins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 21:30:52 by rmartins          #+#    #+#             */
-/*   Updated: 2021/01/13 23:13:29 by rmartins         ###   ########.fr       */
+/*   Updated: 2021/01/14 09:11:01 by rmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
