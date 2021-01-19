01a93236947debef0fc30cd684bbe37232cdcbe8
Romeu Carvalho <rmartins@student.42.fr>
2021-01-19 10:05:23 +0000

    ft_lstdelone

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rmartins <rmartins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 21:30:52 by rmartins          #+#    #+#             */
-/*   Updated: 2021/01/19 09:29:23 by rmartins         ###   ########.fr       */
+/*   Updated: 2021/01/19 09:53:22 by rmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void			ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
 void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void*));
 
 #endif
